9da5c7e7261215bbdd02102c20cd8f0383108638
rameight <yadsendew@protonmail.com>
2023-12-19 20:48:19 +0700

    remove redundant lib

--- types/src/bandwidth.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct UserBandwidthPrice {
-    pub user_id: i64,
-    pub rate_per_kb: i64,
-    pub rate_per_second: i64,
-}
--- types/src/sg_meta.rs
+++
@@ -1,6 +0,0 @@
-pub struct SgMeta {
-    pub id: u32,
-    pub c_head: u64,
-    pub b_from: u64,
-    pub b_to: u64,
-}
