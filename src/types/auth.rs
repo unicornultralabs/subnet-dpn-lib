3199349559c6d6720dcd554fe15f0c490ca9d41b
rameight <yadsendew@protonmail.com>
2023-12-20 02:16:50 +0700

    add user claims

--- src/types/auth.rs
+++ src/types/auth.rs
@@ -1,4 +1,15 @@
-#[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
+use serde::{Deserialize, Serialize};
+
+use super::user::User;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UserClaims {
+    pub user_id: i64,
+    pub user: User,
+    pub exp: u64,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct VerifyAuthTokenReq {
     pub access_token: String,
     pub refresh_token: String,
