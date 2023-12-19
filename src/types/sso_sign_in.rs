0e32f4f2685612e7720ffaeba85699934bc911bd
rameight <yadsendew@protonmail.com>
2023-12-19 20:59:56 +0700

    keep types only

--- src/types/sso_sign_in.rs
+++ src/types/sso_sign_in.rs
@@ -1,5 +1,4 @@
 use num_derive::FromPrimitive;
-use num_traits::FromPrimitive;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, FromPrimitive, Serialize, Deserialize)]
@@ -11,8 +10,8 @@ pub enum SSOMethod {
 
 #[derive(serde::Deserialize, Debug, Clone)]
 pub struct SSOReq {
-    method: i16,
-    payload: String,
+    pub method: i16,
+    pub payload: String,
 }
 
 #[derive(serde::Deserialize, Debug, Clone)]
@@ -36,26 +35,4 @@ pub struct AppleSSOInfo {
     pub full_name: String,
     pub code: String,
     pub token: String,
-}
-
-pub fn parse_sso_sign_in_req(req: SSOReq) -> Result<SSOInfo, String> {
-    match SSOMethod::from_i16(req.method) {
-        Some(method) => match method {
-            SSOMethod::Google => match serde_json::from_str::<GoogleSSOInfo>(&req.payload)
-            {
-                Ok(info) => Ok(SSOInfo::Google(info)),
-                Err(_) => Err(format!("invalid sign in payload")),
-            },
-            SSOMethod::Apple => match serde_json::from_str::<AppleSSOInfo>(&req.payload) {
-                Ok(info) => Ok(SSOInfo::Apple(info)),
-                Err(_) => Err(format!("invalid sign in payload")),
-            },
-        },
-        None => Err(format!("unknown sign in method")),
-    }
-}
-
-pub fn verify_sso_info(_: SSOInfo) -> Result<(), String> {
-    // TODO(rameight): verify sso info
-    Ok(())
 }
