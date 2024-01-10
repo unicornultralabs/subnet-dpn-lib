2162332b1afcf20087f21a3c27ae40637aab5745
ninhnam <ninhnam027@gmail.com>
2024-01-10 09:54:00 +0700

    add region info

---
+++ src/types/region.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+use web3::types::Address;
+
+#[derive(Debug, Serialize, Deserialize, Clone, ToSchema)]
+pub struct UserRegionInfo {
+    pub user_addr: String,
+    pub city_geoname_id: u32,
+    pub country_geoname_id: u32,
+}
+
+impl UserRegionInfo {
+    pub fn new(
+        user_addr: String,
+        city_geoname_id: u32,
+        country_geoname_id: u32,
+    ) -> Self {
+        Self {
+            user_addr: user_addr,
+            city_geoname_id: city_geoname_id,
+            country_geoname_id: country_geoname_id,
+        }
+    }
+}
