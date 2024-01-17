d0388eb751d48501f44c1af293220266aa54e49f
rameight <yadsendew@protonmail.com>
2024-01-17 23:52:07 +0700

    modify proxy acc model

--- src/types/connection.rs
+++ src/types/connection.rs
@@ -1,7 +1,8 @@
-use std::{net::IpAddr, time::Duration};
-
+use dpn_proto::proxy_acc::ProtoProxyAcc;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
+
+use crate::utils::hash::hash;
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub enum ConnectionEvent {
@@ -32,11 +33,59 @@ pub struct PeerStats {
 
 #[derive(Debug, Clone, Deserialize, Serialize, ToSchema)]
 pub struct ProxyAccData {
-    pub client_id: String,
-    pub username: String,
+    pub id: String,
     pub password: String,
-    pub ip_rotation_period: Duration,
-    pub whitelist_ip_list: Vec<IpAddr>,
+    pub ip_rotation_period: i64,
+    pub whitelisted_ip: String,
+    pub user_addr: String,
+    pub country_geoname_id: i64,
+    pub city_geoname_id: i64,
+    pub rate_per_kb: i64,
+    pub rate_per_second: i64,
+    pub created_at: i64,
+}
+
+impl ProxyAccData {
+    pub fn new(
+        password: String,
+        ip_rotation_period: i64,
+        whitelisted_ip: String,
+        user_addr: String,
+        country_geoname_id: i64,
+        city_geoname_id: i64,
+        rate_per_kb: i64,
+        rate_per_second: i64,
+        created_at: i64,
+    ) -> Self {
+        let mut _self = Self {
+            id: "".to_string(),
+            user_addr,
+            password,
+            ip_rotation_period,
+            whitelisted_ip,
+            country_geoname_id,
+            city_geoname_id,
+            rate_per_kb,
+            rate_per_second,
+            created_at,
+        };
+
+        let proto: ProtoProxyAcc = _self.clone().into();
+        let binding = ::prost::Message::encode_to_vec(&proto);
+        let bz = binding.as_slice();
+
+        _self.id = hash(bz).to_string();
+        _self
+    }
+}
+
+impl Into<ProtoProxyAcc> for ProxyAccData {
+    fn into(self) -> ProtoProxyAcc {
+        ProtoProxyAcc {
+            user_addr: self.user_addr,
+            created_at: self.created_at,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize, ToSchema)]
