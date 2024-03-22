20976cca6b280f000c101d74628cdd4bf7ebfa1b
Nguyen Viet Tien <tiennv@uniultra.xyz>
2024-03-22 11:01:11 +0700

    ft: add created_at field

--- src/types/noti.rs
+++ src/types/noti.rs
@@ -3,14 +3,21 @@ pub struct Notification {
     pub id: String,
     pub header: String,
     pub content: String,
+    pub created_at: i64,
 }
 
 impl Notification {
-    pub fn new(id: String, header: String, content: String) -> Self {
+    pub fn new(
+        id: String,
+        header: String,
+        content: String,
+        created_at: i64,
+    ) -> Self {
         Self {
             id,
             header,
             content,
+            created_at,
         }
     }
 }
