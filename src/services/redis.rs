7a54d71ddd1bb2c849e029184e011425b303d73d
Phat Luu <yadsendew@protonmail.com>
2024-04-23 18:58:18 +0700

    add proxy acc

--- src/services/redis.rs
+++ src/services/redis.rs
@@ -5,10 +5,12 @@ use redis::Commands as _;
 use std::{fmt::Debug, sync::Arc};
 
 use crate::types::bandwidth::UserBandwidthPrice;
+use crate::types::connection::ProxyAccData;
 use crate::types::geo::Geo;
 
 const PROVIDER_PRICE_KEY: &str = "provider.price";
 const PROVIDER_GEO_KEY: &str = "provider.geo";
+const PROXY_ACC_KEY: &str = "proxyacc";
 
 #[automock]
 #[async_trait]
@@ -26,12 +28,8 @@ pub trait RedisService: Debug + Send + Sync + 'static {
     async fn set_provider_geo(self: Arc<Self>, provider_addr: String, geo: Geo) -> Result<()>;
     async fn get_provider_geo(self: Arc<Self>, provider_addr: String) -> Result<Geo>;
 
-    async fn set_proxy_acc(
-        self: Arc<Self>,
-        login_session_id: String,
-        geo: Geo,
-    ) -> Result<(), Error>;
-    async fn get_proxy_acc(self: Arc<Self>, login_session_id: String) -> Result<Geo, Error>;
+    async fn set_proxy_acc(self: Arc<Self>, proxy_acc: ProxyAccData) -> Result<(), Error>;
+    async fn get_proxy_acc(self: Arc<Self>, proxy_acc_id: String) -> Result<ProxyAccData, Error>;
 }
 
 #[derive(Debug)]
@@ -124,44 +122,40 @@ impl RedisService for RedisServiceImpl {
         Ok(geo)
     }
 
-    async fn set_proxy_acc(
-        self: Arc<Self>,
-        login_session_id: String,
-        geo: Geo,
-    ) -> Result<(), Error> {
+    async fn set_proxy_acc(self: Arc<Self>, proxy_acc: ProxyAccData) -> Result<(), Error> {
         let mut conn = self
             .client
             .get_connection()
             .map_err(|e| anyhow!("cannot get connection err={}", e))?;
 
         match conn.hset::<String, String, String, usize>(
-            String::from(PROVIDER_GEO_KEY),
-            login_session_id,
-            serde_json::to_string(&geo).unwrap(),
+            String::from(PROXY_ACC_KEY),
+            proxy_acc.id.clone(),
+            serde_json::to_string(&proxy_acc).unwrap(),
         ) {
             Ok(_) => Ok(()),
-            Err(e) => Err(anyhow!("redis failed to insert geography err={}", e)),
+            Err(e) => Err(anyhow!("redis failed to insert proxy acc err={}", e)),
         }
     }
 
-    async fn get_proxy_acc(self: Arc<Self>, login_session_id: String) -> Result<Geo, Error> {
+    async fn get_proxy_acc(self: Arc<Self>, proxy_acc_id: String) -> Result<ProxyAccData, Error> {
         let mut conn = self
             .client
             .get_connection()
             .map_err(|e| anyhow!("cannot get connection err={}", e))?;
-        let geo_str: String = conn
-            .hget(PROVIDER_GEO_KEY, login_session_id.clone())
-            .map_err(|e| {
-                anyhow!(
-                    "redis cannot get key={}:{} err={}",
-                    PROVIDER_PRICE_KEY,
-                    login_session_id,
-                    e
-                )
-            })?;
-        let geo = serde_json::from_str::<Geo>(&geo_str)
+        let proxy_acc_str: String =
+            conn.hget(PROXY_ACC_KEY, proxy_acc_id.clone())
+                .map_err(|e| {
+                    anyhow!(
+                        "redis cannot get key={}:{} err={}",
+                        PROXY_ACC_KEY,
+                        proxy_acc_id,
+                        e
+                    )
+                })?;
+        let proxy_acc = serde_json::from_str::<ProxyAccData>(&proxy_acc_str)
             .map_err(|e| anyhow!("redis failed to decode geography err={}", e))?;
-        Ok(geo)
+        Ok(proxy_acc)
     }
 }
 
