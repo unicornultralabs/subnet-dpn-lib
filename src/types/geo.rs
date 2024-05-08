bc8e5c895ef41f056c1b72a9542870f43f8a8a18
ninhdangthanh <ninhnam027@gmail.com>
2024-05-08 17:13:01 +0700

    add default peer geo

--- src/types/geo.rs
+++ src/types/geo.rs
@@ -38,3 +38,55 @@ pub struct Location {
     pub metro_code: Option<u16>,
     pub time_zone: Option<String>,
 }
+
+impl Default for Geo {
+    fn default() -> Self {
+        Self {
+            city: Some(City::default()),
+            continent: Some(Continent::default()),
+            country: Some(Country::default()),
+            location: Some(Location::default()),
+        }
+    }
+}
+
+impl Default for Continent {
+    fn default() -> Self {
+        Self {
+            code: Some("Default code".to_string()),
+            geoname_id: Some(0),
+            name: Some("Default continent name".to_string()),
+        }
+    }
+}
+
+impl Default for Country {
+    fn default() -> Self {
+        Self {
+            geoname_id: Some(0),
+            is_in_european_union: Some(false),
+            iso_code: Some("Default iso_code".to_string()),
+            name: Some("Default country name".to_string()),
+        }
+    }
+}
+
+impl Default for City {
+    fn default() -> Self {
+        Self {
+            geoname_id: Some(0),
+            name: Some("Default city".to_string()),
+        }
+    }
+}
+
+impl Default for Location {
+    fn default() -> Self {
+        Self {
+            latitude: Some(0.0),
+            longitude: Some(0.0),
+            metro_code: Some(0),
+            time_zone: Some("Default timezone".to_string()),
+        }
+    }
+}
