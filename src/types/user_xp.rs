e13066182f1f87619160ffe40a702494699b30ec
Nguyen Viet Tien <tiennv@uniultra.xyz>
2024-05-16 11:45:40 +0700

    ft: update storage type

--- src/types/user_xp.rs
+++ src/types/user_xp.rs
@@ -5,7 +5,7 @@ use utoipa::ToSchema;
 #[derive(Debug, Serialize, Deserialize, Clone, ToSchema)]
 pub struct UserXp {
     pub user_addr: Address,
-    pub up_time: i64,
+    pub minutes_uptime: f64,
     pub created_at: i64,
     pub updated_at: i64,
 }
@@ -13,13 +13,13 @@ pub struct UserXp {
 impl UserXp {
     pub fn new(
         user_addr: Address,
-        up_time: i64,
+        minutes_uptime: f64,
         created_at: i64,
         updated_at: i64,
     ) -> Self {
         Self {
             user_addr,
-            up_time,
+            minutes_uptime,
             created_at,
             updated_at,
         }
