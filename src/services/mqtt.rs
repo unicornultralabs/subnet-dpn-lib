11f30e2898e754be24424520a4cc254a622ce1fa
Phat Luu <yadsendew@protonmail.com>
2024-05-17 19:09:18 +0700

    remove async

--- src/services/mqtt.rs
+++ src/services/mqtt.rs
@@ -5,7 +5,7 @@ use std::time::Duration;
 pub struct MqttService {}
 
 impl MqttService {
-    pub async fn new(url: String, client_id: String) -> anyhow::Result<(AsyncClient, EventLoop)> {
+    pub fn new(url: String, client_id: String) -> anyhow::Result<(AsyncClient, EventLoop)> {
         let url = format!("{}?client_id={}", url, client_id);
         let mut options = MqttOptions::parse_url(url.clone())
             .map_err(|e| anyhow!("cannot get parse url={} err={}", url, e))?;
