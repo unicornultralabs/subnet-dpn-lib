6c042b6954bb60e59565685ff9d0c3989d9a942e
Phat Luu <yadsendew@protonmail.com>
2024-05-17 19:07:12 +0700

    put to struct

--- src/services/mqtt.rs
+++ src/services/mqtt.rs
@@ -2,9 +2,13 @@ use anyhow::anyhow;
 use rumqttc::v5::{AsyncClient, EventLoop, MqttOptions};
 use std::time::Duration;
 
-pub async fn new(url: String) -> anyhow::Result<(AsyncClient, EventLoop)> {
-    let mut options = MqttOptions::parse_url(url.clone())
-        .map_err(|e| anyhow!("cannot get parse url={} err={}", url, e))?;
-    options.set_keep_alive(Duration::from_secs(10));
-    Ok(AsyncClient::new(options, 10))
+pub struct MqttService {}
+
+impl MqttService {
+    pub async fn new(url: String) -> anyhow::Result<(AsyncClient, EventLoop)> {
+        let mut options = MqttOptions::parse_url(url.clone())
+            .map_err(|e| anyhow!("cannot get parse url={} err={}", url, e))?;
+        options.set_keep_alive(Duration::from_secs(10));
+        Ok(AsyncClient::new(options, 10))
+    }
 }
