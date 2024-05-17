77ca8d02a178040dbd4fb21de1bfb4053f2086bf
Phat Luu <yadsendew@protonmail.com>
2024-05-17 11:29:43 +0700

    add login session id

--- src/services/types.rs
+++ src/services/types.rs
@@ -10,17 +10,20 @@ pub enum PeerStatus {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PeerStatusConnected {
     pub uuid: String,
+    pub login_session_id: String,
     pub ip_u32: u32,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PeerStatusDisconnected {
     pub uuid: String,
+    pub login_session_id: String,
     pub ip_u32: u32,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PeerStatusClientRemoved {
     pub uuid: String,
+    pub login_session_id: String,
     pub ip_u32: u32,
 }
