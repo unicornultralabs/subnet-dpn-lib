85873db17ee3a7759e5bf9ce0c6222ee85eeca78
Phat Luu <yadsendew@protonmail.com>
2024-05-17 19:08:04 +0700

    add client id

--- src/services/mqtt.rs
+++ src/services/mqtt.rs
@@ -5,7 +5,8 @@ use std::time::Duration;
 pub struct MqttService {}
 
 impl MqttService {
-    pub async fn new(url: String) -> anyhow::Result<(AsyncClient, EventLoop)> {
+    pub async fn new(url: String, client_id: String) -> anyhow::Result<(AsyncClient, EventLoop)> {
+        let url = format!("{}?client_id={}", url, client_id);
         let mut options = MqttOptions::parse_url(url.clone())
             .map_err(|e| anyhow!("cannot get parse url={} err={}", url, e))?;
         options.set_keep_alive(Duration::from_secs(10));
