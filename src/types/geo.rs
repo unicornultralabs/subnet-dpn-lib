eed8b29cc56256acd0123fb560bab0ce6f5275c5
DangThanhNinh <83289960+ninhdangthanh@users.noreply.github.com>
2024-05-22 13:22:11 +0700

    Feat: add function get_geo_from_ip_address (#40)
    * add function get_geo_from_ip_address
    
    * change mmdb_path

--- src/types/geo.rs
+++ src/types/geo.rs
@@ -1,4 +1,7 @@
 use serde::{Deserialize, Serialize};
+use anyhow::{anyhow, Result};
+use maxminddb::geoip2;
+use std::net::IpAddr;
 
 pub const DEFAULT_CONTINENTAL_CODE: &str = "DEFAULT";
 
@@ -89,4 +92,76 @@ impl Default for Location {
             time_zone: Some("Default timezone".to_string()),
         }
     }
+}
+
+pub fn get_geo_from_ip_address(mmdb_path: String, ip_addr: String) -> Result<Geo> {
+    let ip_addr: IpAddr = ip_addr
+        .clone()
+        .parse()
+        .map_err(|e| anyhow!("parse ip addr failed err={}", e))?;
+
+    let reader = maxminddb::Reader::open_readfile(mmdb_path)
+        .map_err(|e| anyhow!("failed to read mmdb file err={}", e))?;
+
+    let geo = reader.lookup::<geoip2::City>(ip_addr).map_err(|e| {
+        anyhow!(
+            "no mmdb record for ip_addr={} err={}",
+            ip_addr.to_string(),
+            e
+        )
+    })?;
+
+    let mut geo_city: Option<City> = None;
+    let mut geo_country: Option<Country> = None;
+    let mut geo_continent: Option<Continent> = None;
+    let mut geo_location: Option<Location> = None;
+
+    if let Some(city) = geo.city {
+        geo_city = Some(City {
+            geoname_id: city.geoname_id,
+            name: city
+                .names
+                .unwrap_or_default()
+                .get("en")
+                .map(|n| n.to_string()),
+        });
+    }
+    if let Some(country) = geo.country {
+        geo_country = Some(Country {
+            geoname_id: country.geoname_id,
+            name: country
+                .names
+                .unwrap_or_default()
+                .get("en")
+                .map(|n| n.to_string()),
+            is_in_european_union: country.is_in_european_union,
+            iso_code: country.iso_code.map(|i| i.to_string()),
+        });
+    }
+    if let Some(continent) = geo.continent {
+        geo_continent = Some(Continent {
+            geoname_id: continent.geoname_id,
+            name: continent
+                .names
+                .unwrap_or_default()
+                .get("en")
+                .map(|n| n.to_string()),
+            code: continent.code.map(|c| c.to_string()),
+        });
+    }
+    if let Some(location) = geo.location {
+        geo_location = Some(Location {
+            latitude: location.latitude,
+            longitude: location.longitude,
+            metro_code: location.metro_code,
+            time_zone: location.time_zone.map(|t| t.to_string()),
+        });
+    }
+
+    Ok(Geo {
+        city: geo_city,
+        continent: geo_continent,
+        country: geo_country,
+        location: geo_location,
+    })
 }
