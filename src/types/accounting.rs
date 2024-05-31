960c1f9d23b8f82eb8357d4b29f333010df09b28
Phat Luu <yadsendew@protonmail.com>
2024-05-31 17:41:44 +0700

    add proto (de)serialize

--- src/types/accounting.rs
+++ src/types/accounting.rs
@@ -1,4 +1,5 @@
 use dpn_proto::user_balance::ProtoUserBalance;
+use prost::Message;
 
 #[derive(Debug, Clone)]
 pub struct UserBalance {
@@ -6,7 +7,18 @@ pub struct UserBalance {
     pub balance: i64,
 }
 
-impl UserBalance {}
+impl UserBalance {
+    pub fn to_vec(&self) -> Vec<u8> {
+        let proto: ProtoUserBalance = self.clone().into();
+        let binding = ::prost::Message::encode_to_vec(&proto);
+        binding.as_slice().to_owned()
+    }
+
+    pub fn from_bytes(bz: &[u8]) -> Self {
+        let proto = ProtoUserBalance::decode(bz).expect("decode proto stream payload failed");
+        proto.into()
+    }
+}
 
 impl Into<ProtoUserBalance> for UserBalance {
     fn into(self) -> ProtoUserBalance {
