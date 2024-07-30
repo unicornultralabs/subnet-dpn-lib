93e209e88cb548f277b7002adb96874a24a70b01
Nguyen Viet Tien <tiennv@uniultra.xyz>
2024-07-30 11:00:41 +0700

    ft: add vpn user type

---
+++ src/types/vpn_user.rs
@@ -0,0 +1,28 @@
+use utoipa::ToSchema;
+
+#[derive(Debug, Serialize, Deserialize, Clone, ToSchema)]
+pub struct VpnUser {
+    pub email: Option<String>,
+    pub deposit_addr: Address,
+    pub withdrawal_addr: Option<Address>,
+    pub created_at: i64,
+    pub last_login: i64,
+}
+
+impl User {
+    pub fn new(
+        email: Option<String>,
+        deposit_addr: Address,
+        withdrawal_addr: Option<Address>,
+        created_at: i64,
+        last_login: i64,
+    ) -> Self {
+        Self {
+            email,
+            deposit_addr,
+            withdrawal_addr,
+            created_at,
+            last_login,
+        }
+    }
+}
