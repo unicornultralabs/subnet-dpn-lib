af7d1a0d1a89391ad0b8a913d557c0b67af5c258
Nguyen Viet Tien <157468006+tiennv-u2u@users.noreply.github.com>
2024-07-30 15:32:59 +0700

    Ft/vpn user (#56)
    * ft: add vpn user type
    
    * chore: add missing import
    
    * fix: update import

---
+++ src/types/vpn_user.rs
@@ -0,0 +1,30 @@
+use ethers::types::Address;
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+
+#[derive(Debug, Serialize, Deserialize, Clone, ToSchema)]
+pub struct VpnUser {
+    pub email: Option<String>,
+    pub deposit_addr: Address,
+    pub withdrawal_addr: Option<Address>,
+    pub created_at: i64,
+    pub last_login: i64,
+}
+
+impl VpnUser {
+    pub fn new(
+        email: Option<String>,
+        deposit_addr: Address,
+        withdrawal_addr: Option<Address>,
+        created_at: i64,
+        last_login: i64,
+    ) -> Self {
+        Self {
+            email,
+            deposit_addr,
+            withdrawal_addr,
+            created_at,
+            last_login,
+        }
+    }
+}
